9058eef9674d4aea2af58576a2f6f250f159d5e6
Valentina Morales <valentinacmorales@hotmail.com>
2019-01-09 20:56:14 -0400

    commit

--- client/src/app/signup/signup.component.ts
+++ client/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '../service/http.service';
-import { User } from './../models/user';
+import { User } from '../models/user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
