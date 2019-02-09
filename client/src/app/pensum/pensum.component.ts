1bc2bc810e53249b64b2081ca0df67dedc3d76dc
Valentina Morales <valentinacmorales@hotmail.com>
2019-02-09 15:14:04 -0400

    subject connections

--- client/src/app/pensum/pensum.component.ts
+++ client/src/app/pensum/pensum.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialogConfig, MatDialog } from '@angular/material';
 import { CreatePensumComponent } from '../create-pensum/create-pensum.component';
+import { jsPlumb } from 'jsplumb';
 
 export interface Section {
   hc: number;
   name:string;
   code: string;
+  id: any;
 }
 
 @Component({
@@ -16,34 +18,74 @@ export interface Section {
   styleUrls: ['./pensum.component.scss']
 })
 
-export class PensumComponent implements OnInit {
+export class PensumComponent implements OnInit, AfterViewInit {
+
+  jsPlumbInstance;
+  showConnectionToggle = false;
+  buttonName = 'Connect';
 
   showFiller = false;
 
   shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 
-  constructor(private router: Router, private dialog: MatDialog) { }
+  constructor(private router: Router, private dialog: MatDialog) {}
+
+   ngAfterViewInit() {
+    this.jsPlumbInstance = jsPlumb.getInstance();
+   }
+
+   showConnectOnClick() {
+    this.showConnectionToggle = ! this.showConnectionToggle;
+    if ( this.showConnectionToggle) {
+    this.buttonName = 'Dissconnect';
+      this.connectSourceToTargetUsingJSPlumb();
+    } else {
+      this.buttonName = 'Connect';
+      this.jsPlumbInstance.reset();
+
+      // var conn = this.jsPlumbInstance.connect({source:"element1", target:"element2"});
+      // this.jsPlumbInstance.remove("element1");
+
+      // var conn = this.jsPlumbInstance.connect({source:"element1", target:"element2"});
+      // this.jsPlumbInstance.detach(conn);
+    }
+  }
+
+
+
+  connectSourceToTargetUsingJSPlumb() {
+    // let labelName;
+    //   labelName = 'connection';
+      this.jsPlumbInstance.connect({
+        connector: ['Flowchart', {stub: [10, 10], cornerRadius: 4, alwaysRespectStubs: true}],
+        endpointStyle:{fill:'transparent'},
+        source: 'Source',
+        target: 'Target1',
+        anchor: ['Right', 'Left'],
+        paintStyle: {stroke: '#456', strokeWidth: 2},
+      });  
+  }
 
   goToHome(){
     this.router.navigateByUrl('/home');
   }
 
   todo: Section[] = [
-    { code: '201T18', hc:2, name:'Fisica'},
-    { code: '201T18', hc:3, name:'Matematica'},
-    { code: '201T18', hc:5, name:'Qumica'},
-    { code: '201T18', hc:6, name:'Algebra'},
-    { code: '201T18', hc:3, name:'Geometria'},
-    { code: '201T18', hc:2, name:'Matematica'},
+    { code: '201T18', hc:2, name:'Fisica', id: 'Target1'},
+    { code: '201T18', hc:3, name:'Matematica', id: 'Target1'},
+    { code: '201T18', hc:5, name:'Qumica', id: 'Target1'},
+    { code: '201T18', hc:6, name:'Algebra', id: 'Target1'},
+    { code: '201T18', hc:3, name:'Geometria', id: 'Target1'},
+    { code: '201T18', hc:2, name:'Matematica', id: 'Target1'},
   ];
 
   done: Section[] = [
-    { code: '201T18', hc:2, name:'Matematica'},
-    { code: '201T18', hc:3, name:'Matematica'},
-    { code: '201T18', hc:5, name:'Matematica'},
-    { code: '201T18', hc:6, name:'Matematica'},
-    { code: '201T18', hc:3, name:'Matematica'},
-    { code: '201T18', hc:2, name:'Matematica'},
+    { code: '201T18', hc:2, name:'Matematica', id: 'Source'},
+    { code: '201T18', hc:3, name:'Matematica', id: 'Source'},
+    { code: '201T18', hc:5, name:'Matematica', id: 'Target1'},
+    { code: '201T18', hc:6, name:'Matematica', id: 'Source'},
+    { code: '201T18', hc:3, name:'Matematica', id: 'Target1'},
+    { code: '201T18', hc:2, name:'Matematica', id: 'Source'},
   ];
 
   openPensum() {
